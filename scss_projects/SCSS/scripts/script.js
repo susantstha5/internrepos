eab646381d1fc20b31ad87e1ba35c01c13e21c14
Prabin Karki <karkiprabin198@gmail.com>
2025-06-03 14:27:45 +0545

    assignment_pushed

---
+++ scss_projects/SCSS/scripts/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $('.project_container').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
