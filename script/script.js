e51de1817501478a8ca1be2c7662f7fc3b1b2c99
Shresta Maharjan <srestamaharjan13@gmail.com>
2025-06-03 15:22:41 +0545

    initial massage

---
+++ script/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $(".projects-wrapper").slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+  });
