2846b4e90e24471de239e8063bd26b9c1868d38b
sync4thet1c <sauravkshetry444@gmail.com>
2025-06-17 09:21:26 +0545

    Update Company Logo Section

---
+++ Portfolio_Bootstrap/src/assets/js/slider.js
@@ -0,0 +1,24 @@
+// $(document).ready(function(){
+//       $('.logo-carousel').slick({
+//         slidesToShow: 4,
+//         slidesToScroll: 1,
+//         infinite: true,
+//         arrows: true,
+//         autoplay: false,
+//         vertical: false, // Ensure horizontal sliding
+//         responsive: [
+//           {
+//             breakpoint: 768,
+//             settings: {
+//               slidesToShow: 3
+//             }
+//           },
+//           {
+//             breakpoint: 576,
+//             settings: {
+//               slidesToShow: 2
+//             }
+//           }
+//         ]
+//       });
+//     });
