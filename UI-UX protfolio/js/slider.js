f753bacfbb5d838f84f451ae2cbd52527c421570
xxxx <maharjanneha44@gmail.com>
2025-06-18 00:40:19 +0545

    added transition to all the cards and minor changes

---
+++ UI-UX protfolio/js/slider.js
@@ -0,0 +1,40 @@
+// $('.companyLogo').slick({
+//   dots: true,
+//   infinite: false,
+//   speed: 300,
+//   slidesToShow: 4,
+//   slidesToScroll: 4,
+//   prevArrow:
+//     '<button type="button" class="slick-prev prev-btn"><i class="fa-solid fa-angle-left"></i></button>',
+//   nextArrow:
+//     '<button type="button" class="slick-next next-btn"><i class="fa-solid fa-angle-right"></i></button>',
+//   centerPadding: "60px",
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 3,
+//         // infinite: true,
+//         dots: true
+//       }
+//     },
+//     {
+//       breakpoint: 600,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 2
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//       }
+//     }
+//     // You can unslick at a given breakpoint now by adding:
+//     // settings: "unslick"
+//     // instead of a settings object
+//   ]
+// });
