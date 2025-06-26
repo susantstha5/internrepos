c2eb223c1734134f3928885d96566335d097e098
Prabin Karki <karkiprabin198@gmail.com>
2025-06-26 16:14:39 +0545

    updated

--- projectAnimate/script.js
+++ projectAnimate/script.js
@@ -9,3 +9,15 @@ $(document).ready(function () {
     );
   });
 });
+
+function logSectionHeight() {
+  const navbar = document.querySelector("#navBar");
+  const heroContainer = document.querySelector(".hero_container");
+  if (navbar) {
+    // console.log('offsetHeight:', navbar.offsetHeight);
+    heroContainer.style.paddingTop = `${navbar.offsetHeight}px`;
+  }
+}
+
+window.addEventListener("load", logSectionHeight); // on page load
+window.addEventListener("resize", logSectionHeight); // when resized
